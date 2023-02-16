2a6261d43661b53272a30b25acb27f054faee743
colburncodes <colburnsanders@gmail.com>
2023-02-15 21:03:26 -0600

    feat: routing configured

--- express/index.js
+++ express/index.js
@@ -8,3 +8,71 @@ app.listen(PORT, () => {
   // if everything works fine, the console will show which port the application is listening to
   console.log(`App listening at port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.status(200).send(
+    `<html>
+        <body>
+          <p>Response to the signal from distant space</p>
+        </body>
+        </html>`
+  );
+});
+
+app.get("/pokemon", (req, res) => {
+  // make a mutable copy of our pokedex
+  let result = pokemon;
+
+  // filter out all pokemon that aren't of the desired type
+  if (req.query.type) {
+    result = result.filter((item) => item.type === req.query.type);
+  }
+
+  // filter out all pokemon that aren't of the desired stage
+  if (req.query.stage) {
+    result = result.filter((item) => item.stage === req.query.stage);
+  }
+
+  res.send(result);
+});
+
+const animals = [
+  {
+    kind: "dog",
+    breed: "chihuahua",
+  },
+  {
+    kind: "dog",
+    breed: "bloodhound",
+  },
+  {
+    kind: "dog",
+    breed: "german shepherd",
+  },
+  {
+    kind: "cat",
+    breed: "abyssinian",
+  },
+  {
+    kind: "cat",
+    breed: "dwelf",
+  },
+  {
+    kind: "cat",
+    breed: "highlander",
+  },
+];
+
+app.get("/animals", (req, res) => {
+  debugger;
+  let result = animals;
+  if (req.query.kind) {
+    result = result.filter((item) => item.breed === req.query.breed);
+  }
+
+  if (req.query.kind) {
+    result = result.filter((item) => item.kind == req.query.kind);
+  }
+  res.send(result);
+});
+
